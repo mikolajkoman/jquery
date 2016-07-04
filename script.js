83fa64a04a5c93398560ee5d0f74486e1d78b6af
Mike <mikikoman@gamil.com>
2016-07-04 12:16:47 +0200

    fix button

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 
 $(function(){
-  var li = $('nav').find('li').last().css('color', 'blue');
-  console.log(li);
+	var li = $('nav').find('li').last().css('color', 'blue');
+	console.log(li);
 });
+
 $(function(){
   
-  $("ul").append("<li>Nowy element</li>")
-  
-  var firstItem = $('nav').find('li').first();
- 
-  console.log(firstItem.text());
-  
-  firstItem.css("color", "red")
- 
- 
+	$("ul").append("<li>Nowy element</li>")
+	var firstItem = $('nav').find('li').first();
+	console.log(firstItem.text());
+	firstItem.css("color", "red")
 });
 
 $(function(){
 	var span = $("span");
 	span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css("color", "red");
-	};
-});
+		if(index % 2 == 0) {
+			$(element).css("color", "red");
+		};
+	});
 	var paragraphs = $("p");
-paragraphs.each(function(index, element) {
-	var button = "<button class='btn' data-tmp=' + index + '>Click me</button>"
-$(element).append(button) 
-});
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
-});
+	paragraphs.each(function(index, element) {
+		var button = "<button class='btn' data-tmp='" + index + "'>Click me</button>"
+		$(element).append(button) 
+	});
+	$("button").click(function(){
+		alert($(this).attr("data-tmp"));
+	});
 });
